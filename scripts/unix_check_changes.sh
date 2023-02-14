697ad8f51bb42ca7fa4ee5ec31f81508fa78ac6d
RoyZhang2022 <104782736+RoyZhang2022@users.noreply.github.com>
2023-02-14 15:23:41 -0800

    [AT-1262] Update cron job to pass when checks fail (#88)
    * test change
    
    * test windows self-hosted workflow
    
    * narrow trigger condition
    
    * early exit with 0
    
    * fix syntax error
    
    * update powershell return value
    
    * update windows yml file
    
    * test mac workflow
    
    * update mac self-hosted runner workflow
    
    * test mac change
    
    * fix syntax error
    
    * mac test
    
    * check the unix shell script output
    
    * add comments
    
    * verify a fix for if
    
    * restore shell script change
    
    * test mac
    
    * test mac for continue case
    
    * test mac
    
    * mac test with fix
    
    * fix syntax error
    
    * fix syntax error
    
    * test syntax fix
    
    * add print info
    
    * test fix
    
    * add debug info
    
    * add debug info
    
    * change debug info
    
    * add more debug info
    
    * test debug info
    
    * add debug info
    
    * debug
    
    * change debug
    
    * change debug info
    
    * change syntax
    
    * use env variable for a trial
    
    * remove shell space
    
    * add quote for output
    
    * code refine
    
    * update
    
    * debug more
    
    * test new change
    
    * remove double quotes
    
    * test
    
    * try another way
    
    * test
    
    * test
    
    * test
    
    * test again
    
    * test again
    
    * test again
    
    * fix syntax error
    
    * test multiline results
    
    * add more debug info
    
    * test change
    
    * fix error
    
    * try again
    
    * try single word
    
    * test single word
    
    * try use GITHUB_OUTPUT
    
    * try another way
    
    * try another way
    
    * add debug print
    
    * test
    
    * test
    
    * test
    
    * test using output
    
    * add debug info
    
    * test skip case
    
    * test with long output info
    
    * test mac working version
    
    * code refine
    
    * test skip case
    
    * test workflow if condition
    
    * restore self-host-mac code
    
    * test Windows continue case
    
    * test windows
    
    * fix a typo
    
    * correct windows syntax
    
    * update ps1 script
    
    * update windows workflow
    
    * try another way
    
    * test win_check_changes.ps1
    
    * test another way
    
    * test another way
    
    * refine code
    
    * try new change
    
    * test new change
    
    * test
    
    * test
    
    * add space
    
    * test
    
    * try another solution
    
    * fix syntax error
    
    * test
    
    * update
    
    * windows working version
    
    * test again
    
    * enable test
    
    * test skip case
    
    * enable in windows workflow

--- scripts/unix_check_changes.sh
+++ scripts/unix_check_changes.sh
@@ -11,8 +11,7 @@ echo "Now is $curTime, last commit is $lastCommit, gap is $gap"
 if [ $gap -gt 86400 ]
 then
   echo "No changes for more than one day, skip build and test"
-  exit -1
 else
-  echo "There is a change within one day, start build and test"
-  exit 0
+  # success case must contain key word 'continue' which is checked in mac github workflow
+  echo "There is a change within one day, continue build and test"
 fi
--- scripts/win_check_changes.ps1
+++ scripts/win_check_changes.ps1
@@ -4,12 +4,11 @@
 $curTime=[DateTimeOffset]::Now.ToUnixTimeSeconds()
 $lastCommit=git log -1 --format=%cd --date=unix
 $gap=$curTime - $lastCommit
-echo "Now is $curTime, last commit is $lastCommit, gap is $gap"
+Write-Output "Now is $curTime, last commit is $lastCommit, gap is $gap"
 
 if ($gap -gt 86400) {
-  echo "No changes for more than one day, skip build and test"
-  exit 1
+  Write-Output "No changes for more than one day, skip build and test"
 } else {
-  echo "There is a change within one day, start build and test"
-  exit 0
+  # continue case must contain key word 'continue' which is checked in github workflow
+  Write-Output "There is a change within one day, continue build and test"
 }
