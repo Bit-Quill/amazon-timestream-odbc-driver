6de889597b4d3647a8aade752784701dc2c33d7a
RoyZhang2022 <royz@bitquilltech.com>
2023-03-23 18:52:30 -0700

    copyright update to ASF for ignite code

--- src/tests/input/create_credentials_file.ps1
+++ src/tests/input/create_credentials_file.ps1
@@ -1,3 +1,16 @@
+# Copyright <2022> Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
 # retrieve access key id and secret key from environment variables
 $REPO_ROOT = [System.Environment]::GetEnvironmentVariable('REPOSITORY_ROOT')
 $ENV_ACCESS_ID = [System.Environment]::GetEnvironmentVariable('AWS_ACCESS_KEY_ID')
